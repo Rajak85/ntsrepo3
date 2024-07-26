2cef7d57063842c6964e52d17f3c8092d192b1c3
Rajak85 <akrajiak5@gmail.com>
2024-07-26 15:36:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "Tarraform_demo"
-  }
+provider "aws" {
+  region = "ap-south-1"
 }
-
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
